df55178c882f01b0451a548af10855d311fc2946
opossum <ryabkoff@rambler.ru>
2022-11-23 15:17:49 +0500

    6. Прерывание блокированной нити. [#267413]

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,69 @@
+package ru.job4j.thread;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        String fileName = "tmp_" + url.substring(url.lastIndexOf('/') + 1);
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            long start = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long elapsed = System.currentTimeMillis() - start;
+                if (elapsed < speed) {
+                    Thread.sleep(speed - elapsed);
+                }
+                start = System.currentTimeMillis();
+            }
+        } catch (IOException  e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static boolean isSpeedValid(String s) {
+        int value;
+        try {
+            value = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            value = -1;
+        }
+        return value > 0;
+    }
+
+    private static void validation(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Ќекорректное количество параметров!"
+                    + "ѕервым параметром должен быть URL, вторым скорость");
+        }
+        if (!isSpeedValid(args[1])) {
+            throw new IllegalArgumentException("—корость должна быть задана целым положительным числом!");
+        }
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        validation(args);
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}

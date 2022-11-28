5df79eca5171368c41f918aab3a6268d7864663b
opossum <ryabkoff@rambler.ru>
2022-11-28 15:52:31 +0500

    3. Immutable объекты [#267918 #364249]

--- src/main/java/ru/job4j/Count.java
+++ src/main/java/ru/job4j/Count.java
@@ -3,11 +3,15 @@ package ru.job4j;
 public class Count {
     private int value;
 
-    public synchronized void increment() {
-        value++;
+    public void increment() {
+        synchronized (this) {
+            value++;
+        }
     }
 
-    public synchronized int get() {
-        return value;
+    public int get() {
+        synchronized (this) {
+            return value;
+        }
     }
 }

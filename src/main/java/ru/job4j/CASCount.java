5418c358898b37d2fba172085b88f75d16b0a9ea
opossum <ryabkoff@rambler.ru>
2022-12-07 09:32:54 +0500

    0. CAS - операции [#6859 #368295]

--- src/main/java/ru/job4j/CASCount.java
+++ src/main/java/ru/job4j/CASCount.java
@@ -6,11 +6,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>();
-
-    public CASCount() {
-        count.lazySet(0);
-    }
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
         Integer value;

6ac28cbb41997a91857e78ec58abb004212e75db
opossum <ryabkoff@rambler.ru>
2022-12-07 11:58:53 +0500

    0. CAS - операции [#6859 #368295]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,34 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        return memory.computeIfPresent(model.getId(), (id, base) -> {
+            Base stored = memory.get(model.getId());
+            if (stored.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal");
+            }
+            return new Base(id, base.getVersion() + 1, model.getName());
+        }) != null;
+    }
+
+    public void delete(Base model) {
+        memory.remove(model.getId());
+    }
+
+    public Base get(int id) {
+        return memory.get(id);
+    }
+
+    public int size() {
+        return memory.size();
+    }
+}
